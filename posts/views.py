b2f804a18722c66e74a4e42e0dcfc8f31f7ed7d8
taoist <taoist@shinhan-cubeon.com>
2022-11-22 18:01:03 +0900

    Initial commit

---
+++ posts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render , redirect
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, 'posts/post_list.html' , context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {"post" : post}
+    return render(request, 'posts/post_detail.html', context)
+
+def post_create(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        new_post = post_form.save()
+        return redirect('post-detail', post_id=new_post.id)
+    else:
+        post_form = PostForm()
+        return render(request, 'posts/post_form.html',{'form':post_form})
