1a37d6c50d6cc836ba007cd711a36645b2b4f140
taoist <taoist@shinhan-cubeon.com>
2022-11-26 17:59:05 +0900

    저장

--- posts/views.py
+++ posts/views.py
@@ -1,24 +1,57 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
+from django.http import Http404
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.all()
-    context = {"posts": posts}
-    return render(request, 'posts/post_list.html' , context)
+    paginator = Paginator(posts,6)
+    curr_page_number = request.GET.get('page')
+    if curr_page_number is None:
+        curr_page_number = 1
+    page = paginator.page(curr_page_number)
+    return render(request, 'posts/post_list.html', {'page':page})
 
 def post_detail(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id  )
+    # try:
+    #     post = Post.objects.get(id=post_id)
+    # except Post.DoesNotExist:
+    #     raise Http404()
     context = {"post" : post}
     return render(request, 'posts/post_detail.html', context)
 
 def post_create(request):
     if request.method == 'POST':
         post_form = PostForm(request.POST)
-        new_post = post_form.save()
-        return redirect('post-detail', post_id=new_post.id)
+        if post_form.is_valid():
+            new_post = post_form.save()
+            return redirect('post-detail', post_id=new_post.id)
     else:
         post_form = PostForm()
-        return render(request, 'posts/post_form.html',{'form':post_form})
+    return render(request, 'posts/post_form.html',{'form':post_form})
+
+def post_update(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        post_form = PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('post-detail', post_id=post.id)
+    else:
+        post_form = PostForm(instance=post)
+    return render(request, 'posts/post_form.html', {'form':post_form})
+
+def post_delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method=='POST':
+        post.delete()
+        return redirect('post-list')
+    else:
+        return render(request, 'posts/post_confirm_delete.html', {'post': post})
+
+def index(request):
+    return redirect('post-list')
